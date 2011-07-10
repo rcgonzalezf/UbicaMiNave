a2895408eebf0893ce8e109b4b2f3fc2653f0d2e
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2011-07-09 17:06:46 -0700

    Creating the first MapViewActivity, adding the correct icons for the application.

---
+++ UbicaMiNave/src/com/dev52/main/view/MapViewActivity.java
@@ -0,0 +1,66 @@
+package com.dev52.main.view;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+public class MapViewActivity extends MapActivity {
+	private MapController mapController;
+	private MapView mapView;
+	private LocationManager locationManager;
+	private RelativeLayout linearLayout;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+     linearLayout = (RelativeLayout) findViewById(R.id.mapViewlayout);
+		mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+		mapView.setStreetView(true);
+		mapController = mapView.getController();
+		mapController.setZoom(14); // Zoom 1 is world view
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+				0, new GeoUpdateHandler());
+        
+    }
+    
+    @Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+
+	public class GeoUpdateHandler implements LocationListener {
+
+		@Override
+		public void onLocationChanged(Location location) {
+			int lat = (int) (location.getLatitude());
+			int lng = (int) (location.getLongitude());
+			GeoPoint point = new GeoPoint(lat, lng);
+			mapController.animateTo(point); //	mapController.setCenter(point);
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+	}
+}
