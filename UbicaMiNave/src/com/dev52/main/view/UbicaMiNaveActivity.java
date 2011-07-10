df5885db67ff7247d65ea347cb4f743894b53d5c
klizarraga <klizarraga@nearsoft.com>
2011-07-09 17:43:35 -0700

    Main Screen view

--- UbicaMiNave/src/com/dev52/main/view/UbicaMiNaveActivity.java
+++ UbicaMiNave/src/com/dev52/main/view/UbicaMiNaveActivity.java
@@ -1,13 +1,26 @@
 package com.dev52.main.view;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class UbicaMiNaveActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main);
+ 
+        final ImageButton imageButton = (ImageButton) findViewById(R.id.ButtonSign);
+        imageButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                //TODO Change current view
+                setContentView(R.layout.next);
+            }
+        });
     }
 }
